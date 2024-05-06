5c82742fa8c4de42cd6f38b88dfdbe497f59d696
patelomserverpc <patelom0910@gmail.com>
2024-05-06 19:09:05 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ExtraCharts from './components/ExtraCharts';
+// import Main from './components/MainComp';
+import TestComp from './components/TestComp';
+
+function App() {
+  
+  return (
+    <>
+    {/* <Main /> */}
+      <TestComp />
+      <ExtraCharts />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainComp.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import * as echarts from 'echarts';
+import { AlphaEchart, EchartPie} from 'alpha-echart-library'
+
+const Main = () => {
+
+    // const dataa = [
+    //     { value: 40, name: 'rose 1' },
+    //     { value: 38, name: 'rose 2' },
+    //     { value: 32, name: 'rose 3' },
+    //     { value: 30, name: 'rose 4' },
+    //     { value: 28, name: 'rose 5' },
+    //     { value: 26, name: 'rose 6' },
+    //     { value: 22, name: 'rose 7' },
+    //     { value: 18, name: 'rose 8' }
+    // ]
+
+    return (
+        <>
+            {/* <div className='row'>
+                <div className='col-lg-4'>
+                    <div className='bar'>
+                        <AlphaEchart />
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-lg-12'>
+                    <div className='bar'>
+                        <EchartPie propdata={dataa} />
+                    </div>
+                </div>
+            </div> */}
+        </>
+    );
+};
+
+export default Main;
